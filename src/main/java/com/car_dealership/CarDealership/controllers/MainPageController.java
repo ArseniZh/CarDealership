95333433ad9dcc4f896d340512373997c193fb5d
ArseniZh <szhirnevich@mail.ru>
2023-04-02 22:08:10 +0300

    add new @GetMapping annotation

--- src/main/java/com/car_dealership/CarDealership/controllers/MainPageController.java
+++ src/main/java/com/car_dealership/CarDealership/controllers/MainPageController.java
@@ -1,14 +1,22 @@
 package com.car_dealership.CarDealership.controllers;
 
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.car_dealership.CarDealership.models.Advertisement;
+import com.car_dealership.CarDealership.repositories.AdvertisementRepository;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainPageController {
-
-    @GetMapping("/")
+    private AdvertisementRepository advertisementRepository;
+    @GetMapping("/advertisements")
     public String mainPage() {
         return "index";
     }
+
+    @GetMapping("/advertisement/{id}")
+    public Advertisement getAdvertisement(@PathVariable("id") Advertisement advertisement) {
+        return advertisement;
+    }
+
 }
