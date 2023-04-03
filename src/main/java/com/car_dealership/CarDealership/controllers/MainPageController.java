aba16bad2a9eb49564ffa23e397c9f2cac957a7d
ArseniZh <szhirnevich@mail.ru>
2023-04-03 16:18:57 +0300

    refactor: use AdvertisementService and it's methods

--- src/main/java/com/car_dealership/CarDealership/controllers/MainPageController.java
+++ src/main/java/com/car_dealership/CarDealership/controllers/MainPageController.java
@@ -1,20 +1,19 @@
 package com.car_dealership.CarDealership.controllers;
 
 import com.car_dealership.CarDealership.models.Advertisement;
-import com.car_dealership.CarDealership.repositories.AdvertisementRepository;
+import com.car_dealership.CarDealership.services.AdvertisementService;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
 public class MainPageController {
-    private final AdvertisementRepository advertisementRepository;
+    private AdvertisementService advertisementService;
     @GetMapping("/advertisements")
     public Iterable<Advertisement> getAllAdvertisements() {
-        return advertisementRepository.findAll();
+        return advertisementService.getAllAdvertisements();
     }
 
     @GetMapping("/advertisements/{id}")
@@ -26,17 +25,16 @@ public class MainPageController {
 
     @PostMapping("/advertisements")
     public Advertisement addAdvertisement(@RequestBody Advertisement advertisement) {
-        return advertisementRepository.save(advertisement);
+        return advertisementService.saveAdvertisement(advertisement);
     }
 
     @PutMapping("/advertisements/{id}")
     public Advertisement updateAdvertisement(@PathVariable("id") Advertisement advertisement, @RequestBody Advertisement updatedAdvertisement) {
-        BeanUtils.copyProperties(updatedAdvertisement, advertisement, "id");
-        return advertisementRepository.save(advertisement);
+        return advertisementService.updateAdvertisement(advertisement, updatedAdvertisement);
     }
 
     @DeleteMapping("/advertisements/{id}")
     public void deleteAdvertisement(@PathVariable("id") Advertisement advertisement) {
-        advertisementRepository.delete(advertisement);
+        advertisementService.deleteAdvertisement(advertisement);
     }
 }
