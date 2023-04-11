55cdbc9746645746716b135ad35850e65a6f5c62
ArseniZh <szhirnevich@mail.ru>
2023-04-11 23:25:31 +0300

    add getters, setters and overwrited(toString, equals, ...) methods

--- src/main/java/com/car_dealership/CarDealership/models/User.java
+++ src/main/java/com/car_dealership/CarDealership/models/User.java
@@ -25,4 +25,108 @@ public class User {
             joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
             inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
     private List<Role> userRoles;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public List<Role> getUserRoles() {
+        return this.userRoles;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserRoles(List<Role> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof User)) return false;
+        final User other = (User) o;
+        if (!other.canEqual((Object) this)) return false;
+        final Object this$id = this.getId();
+        final Object other$id = other.getId();
+        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
+        final Object this$firstName = this.getFirstName();
+        final Object other$firstName = other.getFirstName();
+        if (this$firstName == null ? other$firstName != null : !this$firstName.equals(other$firstName)) return false;
+        final Object this$lastName = this.getLastName();
+        final Object other$lastName = other.getLastName();
+        if (this$lastName == null ? other$lastName != null : !this$lastName.equals(other$lastName)) return false;
+        final Object this$email = this.getEmail();
+        final Object other$email = other.getEmail();
+        if (this$email == null ? other$email != null : !this$email.equals(other$email)) return false;
+        final Object this$password = this.getPassword();
+        final Object other$password = other.getPassword();
+        if (this$password == null ? other$password != null : !this$password.equals(other$password)) return false;
+        final Object this$userRoles = this.getUserRoles();
+        final Object other$userRoles = other.getUserRoles();
+        if (this$userRoles == null ? other$userRoles != null : !this$userRoles.equals(other$userRoles)) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof User;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final Object $id = this.getId();
+        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
+        final Object $firstName = this.getFirstName();
+        result = result * PRIME + ($firstName == null ? 43 : $firstName.hashCode());
+        final Object $lastName = this.getLastName();
+        result = result * PRIME + ($lastName == null ? 43 : $lastName.hashCode());
+        final Object $email = this.getEmail();
+        result = result * PRIME + ($email == null ? 43 : $email.hashCode());
+        final Object $password = this.getPassword();
+        result = result * PRIME + ($password == null ? 43 : $password.hashCode());
+        final Object $userRoles = this.getUserRoles();
+        result = result * PRIME + ($userRoles == null ? 43 : $userRoles.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "User(id=" + this.id + ", firstName=" + this.firstName + ", lastName=" + this.lastName +
+                ", email=" + this.email + ", password=" + this.password + ", userRoles=" + this.userRoles + ")";
+    }
 }
