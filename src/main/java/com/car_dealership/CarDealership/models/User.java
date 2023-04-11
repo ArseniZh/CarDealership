510a4e7f5d0ea3e8b985a0a7065280bc6c4e3274
ArseniZh <szhirnevich@mail.ru>
2023-04-11 20:58:08 +0300

    add model: add User model with fields, getters, setters and overwrited methods

---
+++ src/main/java/com/car_dealership/CarDealership/models/User.java
@@ -0,0 +1,25 @@
+package com.car_dealership.CarDealership.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "firstName")
+    private String firstName;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private List<Role> userRoles;
+}
