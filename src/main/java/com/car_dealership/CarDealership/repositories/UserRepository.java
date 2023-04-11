1e8472b7abb41c218b81fc45dd5c432e7e954bcd
ArseniZh <szhirnevich@mail.ru>
2023-04-11 20:58:57 +0300

    add UserRepository

---
+++ src/main/java/com/car_dealership/CarDealership/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.car_dealership.CarDealership.repositories;
+
+import com.car_dealership.CarDealership.models.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
