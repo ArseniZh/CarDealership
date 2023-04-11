8b53495504d05139e92dd5cb080aa03b2baa7a81
ArseniZh <szhirnevich@mail.ru>
2023-04-11 23:27:31 +0300

    add method: add registerUser method to save user in DB

--- src/main/java/com/car_dealership/CarDealership/services/UserService.java
+++ src/main/java/com/car_dealership/CarDealership/services/UserService.java
@@ -1,14 +1,32 @@
 package com.car_dealership.CarDealership.services;
 
+import com.car_dealership.CarDealership.models.Role;
 import com.car_dealership.CarDealership.models.User;
+import com.car_dealership.CarDealership.repositories.RoleRepository;
 import com.car_dealership.CarDealership.repositories.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private BCryptPasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        Role role = roleRepository.findByName("User");
+        List<Role> roles = new ArrayList<Role>();
+        roles.add(role);
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setUserRoles(roles);
+
+        return userRepository.save(user);
+    }
 
     public Iterable<User> getAll() {
         return userRepository.findAll();
