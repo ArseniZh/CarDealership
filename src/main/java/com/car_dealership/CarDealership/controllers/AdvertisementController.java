96c46256a2c4fdc815b0e0d3757375577cd4b155
ArseniZh <szhirnevich@mail.ru>
2023-04-16 19:13:09 +0300

    refactor: change name to AdvertisementController

--- src/main/java/com/car_dealership/CarDealership/controllers/AdvertisementController.java
+++ src/main/java/com/car_dealership/CarDealership/controllers/AdvertisementController.java
@@ -3,14 +3,13 @@ package com.car_dealership.CarDealership.controllers;
 import com.car_dealership.CarDealership.models.Advertisement;
 import com.car_dealership.CarDealership.services.AdvertisementService;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
-public class MainPageController {
+public class AdvertisementController {
     private AdvertisementService advertisementService;
+
     @GetMapping("/advertisements")
     public Iterable<Advertisement> getAllAdvertisements() {
         return advertisementService.getAllAdvertisements();
@@ -18,8 +17,6 @@ public class MainPageController {
 
     @GetMapping("/advertisements/{id}")
     public Advertisement getAdvertisement(@PathVariable("id") Advertisement advertisement) {
-        final Logger log = LoggerFactory.getLogger(MainPageController.class);
-        log.info(String.valueOf(advertisement));
         return advertisement;
     }
 
