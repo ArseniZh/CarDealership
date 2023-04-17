782bafb4e85ec99d542c638b9d07373999a78b73
ArseniZh <szhirnevich@mail.ru>
2023-04-17 12:47:03 +0300

    add: new method findByEmail

--- src/main/java/com/car_dealership/CarDealership/repositories/UserRepository.java
+++ src/main/java/com/car_dealership/CarDealership/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.car_dealership.CarDealership.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email);
 }
