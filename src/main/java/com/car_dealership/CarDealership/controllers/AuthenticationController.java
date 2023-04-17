691f7f97530b621eb1d13f558c7961b3e59f7368
ArseniZh <szhirnevich@mail.ru>
2023-04-17 13:08:14 +0300

    add: PostMapping for sign up and login

--- src/main/java/com/car_dealership/CarDealership/controllers/AuthenticationController.java
+++ src/main/java/com/car_dealership/CarDealership/controllers/AuthenticationController.java
@@ -1,8 +1,13 @@
 package com.car_dealership.CarDealership.controllers;
 
+import com.car_dealership.CarDealership.dto.AuthenticationDto;
+import com.car_dealership.CarDealership.dto.UserDto;
+import com.car_dealership.CarDealership.models.User;
 import com.car_dealership.CarDealership.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private UserService userService;
     private BCryptPasswordEncoder passwordEncoder;
+
+    @PostMapping("/users/sign-up")
+    public UserDto addUser(@RequestBody User user) {
+        userService.registerUser(user);
+        UserDto userDto = UserDto.DtoFromUser(user);
+        return userDto;
+    }
+
+    @PostMapping("/users/login")
+    public String loginUser(@RequestBody AuthenticationDto authenticationDto) {
+        String email = authenticationDto.getEmail();
+        User user = userService.findByEmail(email);
+
+        if (passwordEncoder.matches(authenticationDto.getPassword(), user.getPassword())) {
+            return "token";
+        }
+        return "no such user";
+    }
 
 }
