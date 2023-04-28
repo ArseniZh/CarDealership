abb261583a40290329c3b69aa59381bf67f4f1f0
ArseniZh <szhirnevich@mail.ru>
2023-04-28 14:15:35 +0300

    check authorization

--- src/main/java/com/car_dealership/CarDealership/controllers/AuthenticationController.java
+++ src/main/java/com/car_dealership/CarDealership/controllers/AuthenticationController.java
@@ -3,8 +3,10 @@ package com.car_dealership.CarDealership.controllers;
 import com.car_dealership.CarDealership.dto.AuthenticationDto;
 import com.car_dealership.CarDealership.dto.UserDto;
 import com.car_dealership.CarDealership.models.User;
+import com.car_dealership.CarDealership.services.CustomUserDetailService;
 import com.car_dealership.CarDealership.services.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,18 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
     private UserService userService;
     private BCryptPasswordEncoder passwordEncoder;
+    private CustomUserDetailService userDetailService;
 
     @PostMapping("/users/sign-up")
     public UserDto addUser(@RequestBody User user) {
         userService.registerUser(user);
-        UserDto userDto = UserDto.DtoFromUser(user);
+        UserDto userDto = UserDto.createDtoFromUser(user);
         return userDto;
     }
 
     @PostMapping("/users/login")
-    public String loginUser(@RequestBody AuthenticationDto authenticationDto) {
+    public UserDetails loginUser(@RequestBody AuthenticationDto authenticationDto) {
         String email = authenticationDto.getEmail();
         User user = userService.findByEmail(email);
-        return "no such user";
+
+        if (user != null && passwordEncoder.matches(authenticationDto.getPassword(), user.getPassword())) {
+            return userDetailService.loadUserByUsername(email);
+        }
+        return null;
     }
 }
