a1a3cd8b823a5bff1ababfa81fdc41982cf5f8e8
ArseniZh <szhirnevich@mail.ru>
2023-04-28 10:36:33 +0300

    add method: create securityFilterChain

--- src/main/java/com/car_dealership/CarDealership/configuration/SecurityConfiguration.java
+++ src/main/java/com/car_dealership/CarDealership/configuration/SecurityConfiguration.java
@@ -2,12 +2,31 @@ package com.car_dealership.CarDealership.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/users/sign-up").permitAll()
+                .requestMatchers("/users/login").permitAll()
+                .requestMatchers("/admin/**").hasRole("Admin")
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
     }
 }
