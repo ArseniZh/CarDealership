4601ab254b69e44ef6f4dc956a1b432b36d3368a
ArseniZh <szhirnevich@mail.ru>
2023-05-03 16:53:20 +0300

    changed the return value type to AdvertisementDto

--- src/main/java/com/car_dealership/CarDealership/controllers/AdvertisementController.java
+++ src/main/java/com/car_dealership/CarDealership/controllers/AdvertisementController.java
@@ -1,37 +1,55 @@
 package com.car_dealership.CarDealership.controllers;
 
+import com.car_dealership.CarDealership.dto.AdvertisementDto;
 import com.car_dealership.CarDealership.models.Advertisement;
+import com.car_dealership.CarDealership.models.Car;
 import com.car_dealership.CarDealership.services.AdvertisementService;
+import com.car_dealership.CarDealership.services.CarService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @AllArgsConstructor
 public class AdvertisementController {
     private AdvertisementService advertisementService;
+    private CarService carService;
 
     @GetMapping("/advertisements")
-    public Iterable<Advertisement> getAllAdvertisements() {
-        return advertisementService.getAllAdvertisements();
+    public List<AdvertisementDto> getAllAdvertisements() {
+        List<Advertisement> advertisements = advertisementService.getAllAdvertisements();
+        List<Car> cars = carService.getAllCars();
+
+        return IntStream.range(0, advertisements.size())
+                .mapToObj(i -> AdvertisementDto.createDtoFromAdvertisement(advertisements.get(i), cars.get(i)))
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/advertisements/{id}")
-    public Advertisement getAdvertisement(@PathVariable("id") Advertisement advertisement) {
-        return advertisement;
+    public AdvertisementDto getAdvertisement(@PathVariable("id") Advertisement advertisement) {
+        return AdvertisementDto.createDtoFromAdvertisement(advertisement, advertisement.getCar());
     }
 
     @PostMapping("/advertisements")
-    public Advertisement addAdvertisement(@RequestBody Advertisement advertisement) {
-        return advertisementService.saveAdvertisement(advertisement);
+    public AdvertisementDto addAdvertisement(@RequestBody AdvertisementDto advertisementDto) {
+        Advertisement advertisement = advertisementService.saveAdvertisement(advertisementDto);
+        Car car = advertisement.getCar();
+        return AdvertisementDto.createDtoFromAdvertisement(advertisement, car);
     }
 
     @PutMapping("/advertisements/{id}")
-    public Advertisement updateAdvertisement(@PathVariable("id") Advertisement advertisement, @RequestBody Advertisement updatedAdvertisement) {
-        return advertisementService.updateAdvertisement(advertisement, updatedAdvertisement);
+    public AdvertisementDto updateAdvertisement(@PathVariable("id") Advertisement advertisement, @RequestBody AdvertisementDto updatedAdvertisementDto) {
+        Advertisement updatedAdvertisement = advertisementService.updateAdvertisement(advertisement, updatedAdvertisementDto);
+        Car car = updatedAdvertisement.getCar();
+        return AdvertisementDto.createDtoFromAdvertisement(updatedAdvertisement, car);
     }
 
     @DeleteMapping("/advertisements/{id}")
     public void deleteAdvertisement(@PathVariable("id") Advertisement advertisement) {
         advertisementService.deleteAdvertisement(advertisement);
+        carService.deleteCar(advertisement.getCar());
     }
 }
